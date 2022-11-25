fa31ebc00ac004921021a3b2175bc664087d6e2a
휘응 <81683181+wheeeung@users.noreply.github.com>
2022-11-25 09:42:11 +0900

    [Bronze III] Title: 알람 시계, Time: 232 ms, Memory: 18520 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        if(m >= 45){
+            m -= 45;
+        }
+        else{
+            if(h == 0)
+                h = 24;
+            h -= 1;
+            m += 60 - 45;
+        }
+        System.out.println(h + " " + m);
+    }
+}
