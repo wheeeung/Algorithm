e435052f1ddf8712968ef6568881c3525d71c537
휘응 <81683181+wheeeung@users.noreply.github.com>
2023-03-07 19:20:14 +0900

    [level 1] Title: K번째수, Time: 0.21 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] ans = new int[commands.length];
+
+        for(int i = 0; i < commands.length; i++){
+            ArrayList<Integer> list = new ArrayList<>();
+
+            if(commands[i][0] == commands[i][1]){
+                ans[i] = array[commands[i][0] - 1];
+                continue;
+            }
+            for(int j = commands[i][0] - 1; j <= commands[i][1] - 1; j++){
+                list.add(array[j]);
+            }
+
+            Collections.sort(list);
+            ans[i] = list.get(commands[i][2] - 1);
+        }
+        return ans;
+    }
+}
